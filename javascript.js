584c80ecd0b67ff5c2fd47acad03708ad48cdec1
Xilliano <jorth5250@gmail.com>
2026-08-31 10:45:21 -0500

    Added responsiveness
    Slider to change between different filtered images, a zoom in/out, and reset.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 const inputImg = document.querySelector('#image');
 const image = document.querySelector("img");
 const reset = document.querySelector("button[type=reset]");
-const submit = document.querySelector("button[type=submit]");
+const form = document.querySelector("form");
+const slider = document.querySelector('#slider')
 
 inputImg.addEventListener("change", () => {
     const [file] = inputImg.files;
@@ -10,6 +11,32 @@ inputImg.addEventListener("change", () => {
     }
 })
 
+image.addEventListener("dblclick", (event) => {
+    const rect = image.getBoundingClientRect();
+
+    const x = ((event.clientX - rect.left) / rect.width) * 100;
+    const y = ((event.clientY - rect.top) / rect.height) * 100;
+
+    image.style.transformOrigin = `${x}% ${y}%`;
+    image.classList.toggle("zoomed");
+});
+
 reset.addEventListener("click", () => {
     image.src = '';
+    slider.value = 1;
+    slider.style.display = 'none';
+})
+
+form.addEventListener("submit", (event) => {
+    slider.style.display = "block";
+})
+
+slider.addEventListener("change", () => {
+    let value = slider.value;
+    try {
+        image.src = `http://127.0.0.1:8000/get-image/${value-1}`;
+    }
+    catch {
+        console.log('prob index error');
+    }
 })
