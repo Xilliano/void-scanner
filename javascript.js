c7a48346ff76d22ff8a10d51b42624206bbc96de
Xilliano <jorth5250@gmail.com>
2026-08-31 05:21:38 -0700

    Got API endpoints working, time to do some refactoring of backend

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const inputImg = document.querySelector('#image');
 const image = document.querySelector("img");
 const reset = document.querySelector("button[type=reset]");
+const submit = document.querySelector("button[type=submit]");
 
 inputImg.addEventListener("change", () => {
     const [file] = inputImg.files;
